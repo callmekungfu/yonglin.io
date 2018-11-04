f1a854015fd8096ae2b02fdb65204ffa763206b8
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2018-11-03 21:02:07 -0400

    finished contact form front end

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 var controller = new ScrollMagic.Controller();
+var message = {
+  contact: {}
+};
 
 // Project Revealer
 $('.project').each(function () {
@@ -79,18 +82,88 @@ $('.contact-btn').click(function () {
 
 $('.step2').click((evt) => {
   evt.preventDefault();
-  $('.inline-contact h4').addClass('fadeOutUp');
-  setTimeout(() => {
-    $('.step2').addClass("zoomOut")
-  }, 250);
-  setTimeout(() => {
-    $('.inline-contact').hide();
-    $('#contactMessage').removeClass('hidden');
-    setTimeout(()=>{
-      $('#contactMessage').addClass('fadeIn');
-    }, 100);
-  }, 1000)
-  
+  var flag = true;
+  $('.info-prompt input').each((i, ele) => {
+    var content = $(ele).val();
+    var input = $(ele);
+    if (!content.length) {
+      input.addClass('input-error');
+      input.addClass('tada');
+      flag = false;
+      setTimeout(() => {
+        input.removeClass('tada');
+      }, 600);
+    }
+    if (i === 1) {
+      if (!isEmail(content)) {
+        input.addClass('input-error');
+        input.addClass('tada');
+        flag = false;
+      }
+    }
+  });
+  if (flag) {
+    message.contact.name = $('.info-prompt input[name="name"]').val();
+    message.contact.email = $('.info-prompt input[name="email"]').val();
+    message.subject = $(evt.target).attr('data-subject')
+    $('.inline-contact h4').addClass('fadeOutUp');
+    setTimeout(() => {
+      $('.step2').addClass("zoomOut")
+    }, 250);
+    setTimeout(() => {
+      $('.inline-contact').hide();
+      $('#contactMessage').removeClass('hidden');
+      setTimeout(() => {
+        $('#contactMessage').addClass('fadeIn');
+      }, 100);
+    }, 1000);
+  }
+});
+
+$('.contact-info').keyup((evt) => {
+  var $this = $(evt.target);
+  if ($this.val().length) {
+    $this.removeClass('input-error');
+    $this.removeClass('tada');
+  }
+})
+
+$('#contactMessage').bind('input propertychange', () => {
+  if ($('#contactMessage').val().length) {
+    $(".send-btn").removeClass('rotateOut');
+    $(".send-btn").show();
+    $(".send-btn").addClass('rotateIn');
+  } else {
+    $(".send-btn").removeClass('rotateIn');
+    $(".send-btn").addClass('rotateOut');
+  }
+});
+
+$('.send-btn').click((evt) => {
+  var $title = $('.contact-title');
+  var $desc = $('.contact-desc');
+  message.body = $('#contactMessage').val();
+  $('#contactMessage').removeClass('fadeIn')
+  $('#contactMessage').fadeOut("fast");
+  $(".send-btn").removeClass('rotateIn');
+  $(".send-btn").addClass('rotateOut');
+  $title.css('opacity', '0');
+  $desc.css('opacity', '0');
+  $('.contact-btn').css('opacity', '0');
+  $title.text("Message Sent!")
+  $desc.text("I have received your message and will respond to you as soon as possible, thank you for contacting me! :)")
+  $('.contact-btn').hide()
+  $('.contact-prompt').show();
+  $title.removeClass("fadeOutUp");
+  $desc.removeClass("fadeOutUp");
+  setTimeout(function () {
+    $('.response-icon').removeClass('hidden');
+    $('.response-icon').addClass("bounceIn");
+    setTimeout(() => {
+      $title.addClass("fadeInDown");
+      $desc.addClass("fadeInDown");
+    }, 250);
+  }, 500);
 });
 
 function contactStep2() {
@@ -102,4 +175,13 @@ function contactStep2() {
   setTimeout(() => {
     $('.step2').addClass("bounceIn")
   }, 600)
+};
+
+function resetContactPage() {
+
+} 
+
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
 }
