d8f2e1aed84e27b60212b74f6e1f3a8ae1ce1f11
Yong Lin Wang <wangyonglin1999@gmail.com>
2021-02-27 23:20:31 -0500

    feat: learned more about MDX

--- components/ProjectDetails/ProjectDetails.tsx
+++ components/ProjectDetails/ProjectDetails.tsx
@@ -1,5 +1,29 @@
-const ProjectDetailsPage: React.FC = ({ children }) => (
-  <div className="container md:mx-auto">{children}</div>
+import Head from 'next/head';
+
+interface ProjectDetailsMeta {
+  title: string;
+  desc: string;
+}
+
+interface ProjectDetailsProps {
+  meta: ProjectDetailsMeta;
+}
+
+const ProjectDetailsPage: React.FC<ProjectDetailsProps> = ({
+  children,
+  meta,
+}) => (
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <meta name="Description" content={meta.desc}></meta>
+      <title>{meta.title}</title>
+    </Head>
+    <main>
+      <div className="container md:mx-auto">{children}</div>
+    </main>
+  </>
 );
 
 export default ProjectDetailsPage;
